db9fc1d9127f163bc1363604a128d2ed5902912b
Dhiraj Soren <sorendhiraj>
2021-09-24 19:14:53 +0530

    At 'allowed_user' func 'group = request.user.groups.all()[0].name ' gives cls str value

--- ecommerce/decorators.py
+++ ecommerce/decorators.py
@@ -14,7 +14,7 @@ def allowed_users(allowed_roles=[]):
         def wrapper_funtion(request,*args,**kwargs):
             group = None
             if request.user.groups.exists():
-                group = request.user.groups.all()[0]
+                group = request.user.groups.all()[0].name
             if group in allowed_roles:
                 return view_function(request,*args,**kwargs)
             else:
