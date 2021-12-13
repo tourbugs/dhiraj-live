9f1345b126862d44fc8d9fc054e37b820cd2bb9a
Dhiraj Soren <sorendhiraj>
2021-12-13 23:16:21 +0530

    live

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import  User
 # Create your models here.
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(null=True, blank=True)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(default="logo.png",null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
--- mogambo/settings.py
+++ mogambo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ctt$w=8^4&t1m(mof(rgulr-8#8ep2(o0p@=f+t(j0rpy7=25i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dhirajsoren.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -137,3 +137,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dtmyth5@gmail.com'
+EMAIL_HOST_PASSWORD = 'Toolph!123'
