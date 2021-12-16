b47a528531810fad7c847661494ec4e375d2c229
tourbugs <sorendhiraj@gmail.com>
2021-12-16 23:41:11 +0530

    added database conn

--- mogambo/settings.py
+++ mogambo/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mogambo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -91,7 +91,7 @@ DATABASES = {
     }
 }
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -99,6 +99,9 @@ DATABASES = {
     }
 }
 '''
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
