92dd19688eb40f9038cf1386ec5da9fc2bf65310
tourbugs <sorendhiraj@gmail.com>
2021-12-16 22:54:00 +0530

    database to postgresql

--- mogambo/settings.py
+++ mogambo/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mogambo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -90,15 +90,15 @@ DATABASES = {
         'HOST' : 'localhost',
     }
 }
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
